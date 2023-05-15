ffd6893afff34d7be2a5a422531f57a77f5c4633
BillStephens2022 <two4onebill@yahoo.com>
2023-05-15 16:06:58 -0400

    added WTForm for Registration

--- forms.py
+++ forms.py
@@ -5,6 +5,14 @@ from flask_ckeditor import CKEditorField
 
 
 # WTForm
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
